0f93eb29eb4342c523abe645886767418b05d8a6
nelson nelson <lspardo@uniboyaca.edu.com>
2023-05-24 01:34:14 -0500

    se agrego el metodo para leer a los estudiantes que se encuentran en la base de datos

---
+++ gestion_estudiantes/controllers/EstudianteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace estudianteController;
+
+use baseControler\BaseController;
+use conexionDb\ConexionDbController;
+use estudiante\Estudiante;
+
+class EstudianteController extends BaseController
+{
+
+    function create($estudiante)
+    {
+        // $sql = 'insert into estudiante ';
+        // $sql .= '(id,name,username,password) values ';
+        // $sql .= '(';
+        // $sql .= $estudiante->getId() . ',';
+        // $sql .= '"' . $estudiante->getName() . '",';
+        // $sql .= '"' . $estudiante->getUsername() . '",';
+        // $sql .= '"' . $estudiante->getPassword() . '"';
+        // $sql .= ')';
+        // $conexiondb = new ConexionDbController();
+        // $resultadoSQL = $conexiondb->execSQL($sql);
+        // $conexiondb->close();
+        // return $resultadoSQL;
+    }
+
+    function readEstudiantes()
+    {
+        $sql = 'select * from estudiantes';
+        $conexiondb = new ConexionDbController();
+        $resultadoSQL = $conexiondb->execSQL($sql);
+        $estudiantes = [];
+        while ($registro = $resultadoSQL->fetch_assoc()) {
+            $estudiante = new estudiante();
+            $estudiante->setCodigo($registro['codigo']);
+            $estudiante->setNombre($registro['nombres']);
+            $estudiante->setApellido($registro['apellidos']);
+            array_push($estudiantes, $estudiante);
+        }
+        $conexiondb->close();
+        print_r($estudiante);
+        return $estudiantes;
+        
+    }
+
+    function readRow($id)
+    {
+        // $sql = 'select * from estudiante';
+        // $sql .= ' where id=' . $id;
+        // $conexiondb = new ConexionDbController();
+        // $resultadoSQL = $conexiondb->execSQL($sql);
+        // $estudiante = new estudiante();
+        // while ($registro = $resultadoSQL->fetch_assoc()) {
+        //     $estudiante->setId($registro['id']);
+        //     $estudiante->setName($registro['name']);
+        //     $estudiante->setUsername($registro['username']);
+        //     $estudiante->setPassword('');
+        // }
+        // $conexiondb->close();
+        // return $estudiante;
+    }
+
+    function update($id, $estudiante)
+    {
+        // $sql = 'update estudiante set ';
+        // $sql .= 'name="' . $estudiante->getName() . '",';
+        // $sql .= 'username="' . $estudiante->getUsername() . '",';
+        // $sql .= 'password="' . $estudiante->getPassword() . '" ';
+        // $sql .= ' where id=' . $id;
+        // $conexiondb = new ConexionDbController();
+        // $resultadoSQL = $conexiondb->execSQL($sql);
+        // $conexiondb->close();
+        // return $resultadoSQL;
+    }
+
+    function delete($id)
+    {
+        // $sql = 'delete from estudiante where id=' . $id;
+        // $conexiondb = new ConexionDbController();
+        // $resultadoSQL = $conexiondb->execSQL($sql);
+        // $conexiondb->close();
+        // return $resultadoSQL;
+    }
+}
---
+++ gestion_estudiantes/index.php
@@ -0,0 +1,52 @@
+<?php
+require 'models/estudiante.php';
+require 'controllers/conexionDbController.php';
+require 'controllers/baseController.php';
+require 'controllers/EstudianteController.php';
+
+use estudianteController\EstudianteController;
+
+$estudianteController = new EstudianteController();
+
+$estudiantes = $estudianteController->readEstudiantes();
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <h1>Lista de estudiantes</h1>
+        <a href="views/form_estudi$estudiante.php">Registrar estudiante</a>
+        <table>
+            <thead>
+                <tr>
+                    <th>codigo</th>
+                    <th>Nombres</th>
+                    <th>apellidos</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($estudiantes as $estudiante) {
+                    echo '<tr>';
+                    echo '  <td>' . $estudiante->getCodigo() . '</td>';
+                    echo '  <td>' . $estudiante->getNombre() . '</td>';
+                    echo '  <td>' . $estudiante->getApellido() . '</td>';
+                    // echo '  <td>';
+                    // echo '      <a href="views/form_estudi$estudiante.php?id=' . $estudiante->get() . '">modificar</a>';
+                    // echo '      <a href="views/accion_borrar_estudi$estudiante.php?id=' . $estudiante->getId() . '">borrar</a>';
+                    // echo '  </td>';
+                    echo '</tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </main>
+</body>
+
+</html>
